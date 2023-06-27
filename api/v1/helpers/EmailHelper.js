ab8adf10c1bc7f4cbe897553a38a774b509852cb
Denis Gitau <denisgitau12@gmail.com>
2023-06-27 16:57:27 +0300

    Customize password reset dummy function

--- api/v1/helpers/EmailHelper.js
+++ api/v1/helpers/EmailHelper.js
@@ -1,7 +1,15 @@
+const dotenv =require("dotenv");
+dotenv.config();
+
 const send = (phrase) => {
 	console.log(phrase);
 }
 
+const sendPasswordResetToken = (user, token) => {
+	console.log(`Dear ${user.name} reset your password here ${process.env.FRONTEND_DOMAIN}/reset/${token}`);
+}
+
 module.exports = {
 	send,
+	sendPasswordResetToken
 }
