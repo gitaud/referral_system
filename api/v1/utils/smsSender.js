4935bb548670028f540bb8ca1ed2882dfdad29b5
Denis Gitau <denisgitau12@gmail.com>
2023-06-27 15:02:37 +0300

    Modify printed statement

--- api/v1/utils/smsSender.js
+++ api/v1/utils/smsSender.js
@@ -1,13 +1,13 @@
 const sendLevelUpdateMsg = (user, level ) => {
-	console.log(`Dear ${user.name}, Congratulations! Your earnings level has been increased to ${level.name}, with a commission of Ksh.${level.savings} for every Ksh 100 spent`);
+	console.log(`Dear ${user.name}, Congratulations! Your earnings level has been increased to ${level.name}, with a commission of Ksh ${level.savings} for every Ksh 100 spent`);
 }
 
 const sendCommissionPaidMsg = (user, amountPaid) => {
-	console.log(`Dear ${user.name} your earned commission of Ksh.${amountPaid} has been deposited in your account.`)
+	console.log(`Dear ${user.name} your earned commission of Ksh ${amountPaid} has been deposited in your account.`)
 }
 
 const sendCommissionEarnedMsg = (user, amount) => {
-	console.log(`Dear ${user.name} you have earned a commission of Ksh.${amount}. Your total balance is ${user.commissionDue} `)
+	console.log(`Dear ${user.name} you have earned a commission of Ksh ${amount}. Your total balance is ${user.commissionDue} `)
 }
 
 module.exports = { 
