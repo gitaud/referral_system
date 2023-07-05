0b973fdcdc1213f90e06d3a7a6ac2828e8e1b459
Denis Gitau <denisgitau12@gmail.com>
2023-07-05 15:41:17 +0300

    Add initialize login action and persist data in user's local storage

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -19,9 +19,14 @@ try {
   }
 }
 
+export const INITIATING_LOGIN = 'INITIATING LOGIN'
 export const LOGIN_SUCCESS = 'LOGIN SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN FAILURE';
 export const LOGOUT = 'LOGOUT';
+
+export const initiateLogin = () => {
+  return { type: INITIATING_LOGIN }
+}
 
 export const loginSuccess = (data) => {
   return { type: LOGIN_SUCCESS, data }
@@ -36,16 +41,18 @@ export const logout = () => {
 }
 
 const authReducer = (state, action) => {
+  localStorage.setItem('USER_STATE', null);
+  console.log(action);
   switch(action.type) {
+    case INITIATING_LOGIN:
+      return { isLoggedIn: false, authToken: null, name: null, error: null }
     case LOGIN_SUCCESS:
       let userData = { isLoggedIn: true, authToken: action.data.token, name: action.data.name, error: null};
       localStorage.setItem('USER_STATE', JSON.stringify(userData));
       return userData
     case LOGIN_FAILURE:
-      localStorage.setItem('USER_STATE', null);
       return { isLoggedIn: false, authToken: null, name: null, error: action.error }
     case LOGOUT:
-      localStorage.setItem('USER_STATE', null);
       return {
         isLoggedIn: false,
         authToken: null,
