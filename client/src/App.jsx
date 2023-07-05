c944e951cd8ce58c3c7010d188eedc6be7e741c0
Denis Gitau <denisgitau12@gmail.com>
2023-07-05 16:05:07 +0300

    Add password reset request route to app

---
+++ client/src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useLayoutEffect, lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route, Outlet, useNavigate } from 'react-router-dom';
+import './App.css';
+import { AuthContextProvider, useAuthContext } from './context/AuthContext';
+import useSetDocumentTitle from './common-hooks/setDocumentTitle';
+const Sidebar = lazy(() => import('./common-components/sidebar/Sidebar'));
+const Topbar = lazy(() => import( './common-components/topbar/Topbar'));
+const Login = lazy(() => import( './pages/Auth/Login/LoginForm'));
+const RequestPasswordReset = lazy(() => import('./pages/Auth/RequestPasswordReset/RequestPasswordResetForm'));
+
+const Homepage = () => {
+
+	const navigate = useNavigate();
+	const { user } = useAuthContext();
+	useLayoutEffect(() => {
+		if (user.isLoggedIn === false) {
+			navigate("/login");
+		}
+	})
+	useSetDocumentTitle("Home");
+	return(
+		<>
+			<Topbar />
+			<div className="container">
+				<Sidebar />
+				<Outlet />
+			</div>
+		</>
+	)
+}
+
+function App() {
+	return (
+		<AuthContextProvider>
+			<Router>
+				<Suspense fallback={<div><p>Loading</p></div>}>
+					<Routes>
+						<Route path="/" element={ <Homepage />}>
+
+						</Route>
+						<Route path="/login" element={<Login />} />
+						<Route path="/reset/password" element={<RequestPasswordReset />} />
+					</Routes>
+				</Suspense>
+			</Router>
+		</AuthContextProvider>
+	);
+}
+
+export default App;
