d445b7be3aea255600b97e569e7340a7c9273076
Denis Gitau <denisgitau12@gmail.com>
2023-07-05 21:08:43 +0300

    Create user context

---
+++ client/src/context/UserContext.jsx
@@ -0,0 +1,50 @@
+import React, { createContext, useContext, useReducer }  from 'react';
+
+const UserContext = createContext(null);
+// Actions
+
+const INITIALIZE = 'INITIALIZE';
+const SUCCESS = 'SUCCESS';
+const FAILURE = 'FAILURE';
+
+// Action Creators
+
+export const initialize = () => {
+	return { type: INITIALIZE }
+}
+
+export const success = (data) => {
+	return { type: SUCCESS, data }
+}
+
+export const failure = (error) => {
+	return { type: FAILURE, error }
+}
+
+const initialValues = {
+	user: null,
+	error: null
+}
+
+export const userContextReducer = (action, state) => {
+	switch(action.type) {
+		case INITIALIZE:
+			return {};
+		case SUCCESS:
+			return { user: action.user }
+		case FAILURE:
+			return { user: null, error: action.error };
+		default:
+			return state;
+	}
+}
+
+export const UserContextProvider = (props) => {
+	const [ user, dispatch ] = useReducer(userContextReducer, initialValues);
+
+	return(<UserContext.Provider value={{ user, dispatch }} {...props} />)
+}
+
+export const useUserContext = () => {
+	return useContext(UserContext);
+}
