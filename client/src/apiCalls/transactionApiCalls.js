6ac4f2ee86d0a0a60189605f81a2e4eba47620ab
Denis Gitau <denisgitau12@gmail.com>
2023-07-13 21:10:32 +0300

    Create transaction api calls

--- client/src/apiCalls/transactionApiCalls.js
+++ client/src/apiCalls/transactionApiCalls.js
@@ -1,3 +1,28 @@
-const getAllOrders = () => {
-	
+import { userRequest } from './baseApiCall';
+
+export const createTransaction = async (token, data) => {
+	try {
+		const response = await userRequest(token).post("/transaction", data);
+		return response.data;
+	} catch(error) {
+		throw new Error("Could not create transaction", { cause: error });
+	}
+}
+
+export const getOneTransaction = async (token, id) => {
+	try {
+		const response = await userRequest(token).get(`/transactions/${id}`)
+		return response.data;
+	} catch(error) {
+		throw new Error("Could not fetch transaction", { cause: error })
+	}
+}
+
+export const getAllTransactions = async (token, filterParams) => {
+	try {
+		const response = await userRequest(token).get(`/transactions?${filterParams}`)
+		return response.data;
+	} catch(error) {
+		throw new Error("Could not fetch transactions", { cause: error })
+	}
 }
