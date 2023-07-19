f074f349e14f5efa93b1a0652e4b656bd62c3068
Denis Gitau <denisgitau12@gmail.com>
2023-07-19 17:15:30 +0300

    Relax token restriction from superadmin to admin

--- api/v1/routes/MenuRoutes.js
+++ api/v1/routes/MenuRoutes.js
@@ -2,17 +2,17 @@ const router = require("express").Router();
 const MenuController = require("../controllers/MenuController");
 const TokenHelper = require("../helpers/TokenHelper");
 
-router.post("/categories", TokenHelper.verifySuperAdmin, MenuController.createNewCategory);
+router.post("/categories", TokenHelper.verifyAdmin, MenuController.createNewCategory);
 
-router.post("/items", TokenHelper.verifySuperAdmin, MenuController.createNewMenuItem);
+router.post("/items", TokenHelper.verifyAdmin, MenuController.createNewMenuItem);
 
-router.get("/categories", TokenHelper.verifySuperAdmin, MenuController.getAllMenuCategories)
+router.get("/categories", TokenHelper.verifyAdmin, MenuController.getAllMenuCategories)
 
-router.get("/categories/:id", TokenHelper.verifySuperAdmin, MenuController.getMenuCategory)
+router.get("/categories/:id", TokenHelper.verifyAdmin, MenuController.getMenuCategory)
 
-router.patch("/categories/:id", TokenHelper.verifySuperAdmin, MenuController.updateMenuCategoryDetails)
+router.patch("/categories/:id", TokenHelper.verifyAdmin, MenuController.updateMenuCategoryDetails)
 
-router.patch("/items/:id", TokenHelper.verifySuperAdmin, MenuController.updateMenuItem)
+router.patch("/items/:id", TokenHelper.verifyAdmin, MenuController.updateMenuItem)
 
 router.delete("/categories/:id", TokenHelper.verifySuperAdmin, MenuController.deleteMenuCategory);
 
